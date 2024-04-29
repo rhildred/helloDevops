84564b031649f6bbd6378e3722a95f6450b4475b
rhildred <rhildred@gmail.com>
2024-04-29 11:20:04 -0700

    initial commit

---
+++ hello.py
@@ -0,0 +1,4 @@
+print("My name is Rich Hildred")
+print("My favourite role was team leader on a systems integration between bank one and UPS")
+print("""It relates to Devops because a lot had to go right to make the system work. 
+Bank One was very good at operations/ making things go right. I learned a lot.""")
